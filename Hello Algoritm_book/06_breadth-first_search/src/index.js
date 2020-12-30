d08b3d9dc7f1799f5f54bba056fb7111cd775027
gay0ung <6and_end9@naver.com>
2020-12-31 00:07:37 +0900

    06_breadth-first_search

---
+++ Hello Algoritm_book/06_breadth-first_search/src/index.js
@@ -0,0 +1,10 @@
+let graph = {};
+
+graph["you"] = ["alice", "bob", "claire"];
+graph["bob"] = ["anju", "peggy"];
+graph["alice"] = ["peggy"];
+graph["claire"] = ["thom", "jonny"];
+graph["anju"] = [];
+graph["peggy"] = [];
+graph["thom"] = [];
+graph["jonny"] = [];

f790b15edc468a9c174c7c0c739221786765f019
ga young <58800923+gay0ung@users.noreply.github.com>
2025-04-14 16:58:31 +0900

    10. 괄호 회전하기 다시 풀기

--- 코딩테스트완전정복/10.js
+++ 코딩테스트완전정복/10.js
@@ -1,71 +1,108 @@
-function isValidParentheses(stack) {
-    const s_open = "(",
-        s_close = ")",
-        m_open = "{",
-        m_close = "}",
-        l_open = "[",
-        l_close = "]";
+// function isValidParentheses(stack) {
+//     const s_open = "(",
+//         s_close = ")",
+//         m_open = "{",
+//         m_close = "}",
+//         l_open = "[",
+//         l_close = "]";
 
-    let arr = stack;
-    let currentIdx = 0;
+//     let arr = stack;
+//     let currentIdx = 0;
     
-    // 모든 문자열의 괄호가 맞아야함. 그럼 맞는 괄호면 제거하고, 배열이 비여있을때만 true 해주면 될듯
-    while (currentIdx < stack.length) {
-        currentIdx++;
-        let idx = 0;
+//     // 모든 문자열의 괄호가 맞아야함. 그럼 맞는 괄호면 제거하고, 배열이 비여있을때만 true 해주면 될듯
+//     while (currentIdx < stack.length) {
+//         currentIdx++;
+//         let idx = 0;
 
-        if (!stack[idx] || !stack[idx + 1]) {
+//         if (!stack[idx] || !stack[idx + 1]) {
          
-            return;
-        }
+//             return;
+//         }
 
-        console.log("🪬 currentIdx", currentIdx);
-        console.log("stack : ", stack);
-        console.log("Arr : ", arr);
+//         console.log("🪬 currentIdx", currentIdx);
+//         console.log("stack : ", stack);
+//         console.log("Arr : ", arr);
         
-        if (stack[idx] && (
-            stack[idx] === s_open && stack[idx+1] === s_close || 
-            stack[idx] === m_open && stack[idx+1] === m_close ||
-            stack[idx] === l_open && stack[idx+1] === l_close
-        )) {
-            // arr = [...arr, stack[currentIdx], stack[currentIdx+1]];
-            arr.splice(0, 2);
-            console.log(arr);
+//         if (stack[idx] && (
+//             stack[idx] === s_open && stack[idx+1] === s_close || 
+//             stack[idx] === m_open && stack[idx+1] === m_close ||
+//             stack[idx] === l_open && stack[idx+1] === l_close
+//         )) {
+//             // arr = [...arr, stack[currentIdx], stack[currentIdx+1]];
+//             arr.splice(0, 2);
+//             console.log(arr);
             
-            idx = 0;
-        } else {
-            idx++;
-        }
+//             idx = 0;
+//         } else {
+//             idx++;
+//         }
        
-        currentIdx++;
-    }
+//         currentIdx++;
+//     }
 
-    return arr.length === 0;
+//     return arr.length === 0;
+// }
+
+// function solution(s) {
+//     /* 
+//         [()] -> 올바른 괄호이다
+//         shilft 한뒤 push 한 결과의 문자열 모두가 올바른 괄호 형태여야 한다.  
+//     */
+//     let currentIdx = 0;
+//     let answer = 0;
+//     let stack = s.split('');
+
+//     while (currentIdx < s.length) {
+//         const character = stack.shift();
+//         stack.push(character);
+        
+//         currentIdx++;
+
+//         if (isValidParentheses(stack)) {
+//             answer++;
+//         }
+        
+//     }
+
+//     return answer;
+// }
+
+// 내가 풀이한 방법으로 다시 알맞게 풀이해 보기
+function isValidParentheses(s) {
+    const stack = [];
+    const map = {
+        ")" : "(",
+        "}" : "{",
+        "]" : "["
+    }
+    for (let char of s) {
+        if (char === "(" || char === "{" || char === "[") {
+            stack.push(char);
+        } else {
+            if (stack.length === 0 || stack[stack.length - 1] !== map[char]) {
+                return false;
+            }
+            stack.pop();
+        }
+    }
+    return stack.length === 0;
 }
 
 function solution(s) {
-    /* 
-        [()] -> 올바른 괄호이다
-        shilft 한뒤 push 한 결과의 문자열 모두가 올바른 괄호 형태여야 한다.  
-    */
-    let currentIdx = 0;
     let answer = 0;
-    let stack = s.split('');
+    let arr = s.split('');
 
-    while (currentIdx < s.length) {
-        const character = stack.shift();
-        stack.push(character);
-        
-        currentIdx++;
-
-        if (isValidParentheses(stack)) {
+    for (let i = 0; i < s.length; i++) {
+        if (isValidParentheses(arr)) {
             answer++;
         }
-        
+
+        arr.push(arr.shift());
     }
 
     return answer;
 }
+
 
 console.log(solution("[](){}")); // 3
 console.log(solution("}]()[{")); // 2

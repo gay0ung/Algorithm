94ad606de75f55441d272679723882379a051713
ga young <58800923+gay0ung@users.noreply.github.com>
2025-04-21 16:00:52 +0900

    12. 복습하기

--- 코딩테스트완전정복/12.js
+++ 코딩테스트완전정복/12.js
@@ -1,16 +1,33 @@
+// function solution(prices) {
+//     /* 주가가 n초 동안 유지된 시가을 리턴 하면 된다. */
+//     const answer = Array(prices.length).fill(0)
+
+//     for(let i = 0; i < prices.length; i++) {
+//         for(let j = 1; j < prices.length; j++) {
+//             if (prices[i] <= prices[i+j]) {
+//                 answer[i] += 1; 
+//             }
+//         }
+//     }
+
+//     return answer; 
+// }
+
+/* 원인 분석해서 다시 풀어 보기 */
 function solution(prices) {
-    /* 주가가 n초 동안 유지된 시가을 리턴 하면 된다. */
     const answer = Array(prices.length).fill(0)
 
     for(let i = 0; i < prices.length; i++) {
-        for(let j = 1; j < prices.length; j++) {
-            if (prices[i] <= prices[i+j]) {
-                answer[i] += 1; 
+        for(let j = i + 1; j < prices.length; j++) {
+            answer[i]++;
+
+            if(prices[i] > prices[j]) {
+                break;
             }
         }
     }
 
-    return answer; 
+    return answer
 }
 
 console.log(solution([1, 2, 3, 2, 3])); // [4, 3, 1, 1, 0]

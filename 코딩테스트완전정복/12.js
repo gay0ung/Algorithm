e3a3eff2f05925f78eafbfef8d75846d7bd94d9d
ga young <58800923+gay0ung@users.noreply.github.com>
2025-04-21 16:40:14 +0900

    12. 스택으로 풀이하기

--- 코딩테스트완전정복/12.js
+++ 코딩테스트완전정복/12.js
@@ -14,20 +14,40 @@
 // }
 
 /* 원인 분석해서 다시 풀어 보기 */
+// function solution(prices) {
+//     const answer = Array(prices.length).fill(0)
+
+//     for(let i = 0; i < prices.length; i++) {
+//         for(let j = i + 1; j < prices.length; j++) {
+//             answer[i]++;
+
+//             if(prices[i] > prices[j]) {
+//                 break;
+//             }
+//         }
+//     }
+
+//     return answer
+// }
+
+/* 스택으로 풀이 하기 */
 function solution(prices) {
-    const answer = Array(prices.length).fill(0)
+    const answer = Array(prices.length).fill(0);
 
-    for(let i = 0; i < prices.length; i++) {
-        for(let j = i + 1; j < prices.length; j++) {
-            answer[i]++;
+    const stack = [0];
 
-            if(prices[i] > prices[j]) {
-                break;
-            }
+    for (let i = 1; i < prices.length; i++) {
+        while (stack.length > 0 && prices[i] < prices[stack[stack.length - 1]]) {
+            const prevIdx = stack.pop();
+            answer[prevIdx] = i - prevIdx;
         }
+        stack.push(i);
     }
 
-    return answer
+    while (stack.length > 0) {
+        const lastStackIdx = stack.pop();
+        answer[lastStackIdx] = prices.length - 1 - lastStackIdx;
+    }
 }
 
 console.log(solution([1, 2, 3, 2, 3])); // [4, 3, 1, 1, 0]
